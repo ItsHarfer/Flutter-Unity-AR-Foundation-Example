f508889345bf0e5058ce10535701256775e3c75c
Martin Haferanke <martin.haferanke@googlemail.com>
2021-05-03 19:48:55 +0200

    - adding metadata files

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_unity/flutter_unity.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter <> Unity AR Foundation App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter <> Unity AR Foundation App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter with Unity AR Foundation'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => UnityViewPage()));
+          },
+          child: Text('Start Image Tracking'),
+        ),
+      ),
+    );
+  }
+}
+
+class UnityViewPage extends StatefulWidget {
+  @override
+  _UnityViewPageState createState() => _UnityViewPageState();
+}
+
+class _UnityViewPageState extends State<UnityViewPage> {
+  UnityViewController unityViewController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AR Image Detection'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          UnityView(
+            onCreated: onUnityViewCreated,
+            onReattached: onUnityViewReattached,
+            onMessage: onUnityViewMessage,
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: Card(
+              elevation: 10,
+              child: Column(
+                children: <Widget>[
+                  // Overlay Content here if you want
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onUnityViewCreated(UnityViewController controller) {
+    print('onUnityViewCreated');
+
+    unityViewController = controller;
+  }
+
+  void onUnityViewReattached(UnityViewController controller) {
+    print('onUnityViewReattached');
+  }
+
+  void onUnityViewMessage(UnityViewController controller, String message) {
+    print('onUnityViewMessage');
+
+    print(message);
+  }
+}
